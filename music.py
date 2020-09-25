ef2b56d56ba4548a3c0024ae626f90c5b49b9a4d
RobertAndion <randion@usf.edu>
2020-09-25 17:02:25 -0400

    Abuse Prevention
    Massive updates to the functions in music.py to prevent people not in the same voice channel as the bot from modifying the bot. this does not affect the play command. A more extensive implementation will be needed to cover play. I also did not do queue as I did not think it was neccessary. Can add in the future if it proves to be something needed.

--- music.py
+++ music.py
@@ -21,7 +21,7 @@ class music(commands.Cog):
         self.bot.music.add_event_hook(self.track_hook)
 
     
-    @commands.command(name = 'play', description=".play {song name} to play a song, will connect the bot.") #Allows for a song to be played, removed need of join
+    @commands.command(name = 'play', description=".play {song name} to play a song, will connect the bot.") #Allows for a song to be played, does not make sure people are in the same chat. Harder to implement.
     @commands.has_any_role('Dj','Administrator','DJ')
     async def play_song(self, ctx, *, query):
         member = utils.find(lambda m: m.id == ctx.author.id, ctx.guild.members) # This will connect the bot if it is not already connected.
@@ -33,25 +33,21 @@ class music(commands.Cog):
                 await self.connect_to(ctx.guild.id, str(vc.id))
 
         try:
-            if ctx.author.voice is not None: #and ctx.channel.id == vc.id: #make sure sender is in voice chat to play a song
-                #player = self.bot.music.player_manager.get(ctx.guild.id)
-                query = query.strip('<>')
-                if not url_rx.match(query): # This and the line above and below allow for direct link play
-                    query = f'ytsearch:{query}'
+            query = query.strip('<>')
+            if not url_rx.match(query): # This and the line above and below allow for direct link play
+                query = f'ytsearch:{query}'
 
-                results = await player.node.get_tracks(query)
-                try:
-                    track = results['tracks'][0]
-                    player.add(requester=ctx.author.id, track=track)
-                    track_title = track["info"]["title"]
-                    if not player.is_playing:
-                        await player.play()
-                    fileRead.logUpdate(member,track_title) # Add the song to the log
-                    await ctx.channel.send(f"{track_title} added to queue.") 
-                except Exception as error:
-                    await ctx.channel.send("Song not found. (or title has emojis/symbols)")
-            else:
-                await ctx.channel.send("Please join a voice chat to play a song.")
+            results = await player.node.get_tracks(query)
+            try:
+                track = results['tracks'][0]
+                player.add(requester=ctx.author.id, track=track)
+                track_title = track["info"]["title"]
+                if not player.is_playing:
+                    await player.play()
+                fileRead.logUpdate(member,track_title) # Add the song to the log
+                await ctx.channel.send(f"{track_title} added to queue.") 
+            except Exception as error:
+                await ctx.channel.send("Song not found. (or title has emojis/symbols)")
 
         except Exception as error:
             print(error)
@@ -69,69 +65,104 @@ class music(commands.Cog):
     @commands.command(name = 'skip',description="Skips currently playing song.") #skips currently playing song
     @commands.has_any_role('Dj','Administrator','DJ')
     async def skip_song(self, ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if not player.is_playing:
-            await ctx.channel.send("Nothing playing to skip.")
-        else:
-            await player.skip()
-            await ctx.channel.send("Song skipped.")
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if not player.is_playing:
+                    await ctx.channel.send("Nothing playing to skip.")
+                else:
+                    await player.skip()
+                    await ctx.channel.send("Song skipped.")
+            else:
+                await ctx.channel.send("Please join the same voice channel as me.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
     @commands.command(name = "clear",description="Clears all of the currently playing songs and makes the bot disconnect.")
     @commands.has_any_role("Dj","DJ","Administrator")
     async def clear_queue(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if player.is_playing:
-            while player.is_playing:
-                await player.skip()
-            await ctx.channel.send("Songs Cleared.")
-        else:
-            await ctx.channel.send("Nothing playing to clear.")
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if player.is_playing:
+                    while player.is_playing:
+                        await player.skip()
+                    await ctx.channel.send("Songs Cleared.")
+                else:
+                    await ctx.channel.send("Nothing playing to clear.")
+            else: 
+                await ctx.channel.send("Please join the same voice channel as me.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
 
 
     @commands.command(name = 'stop',description="Stops the bot's audio") #stops all music to be played on bot
     @commands.has_any_role('Dj','Administrator','DJ')
     async def stop_song(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if not player.is_playing:
-            ctx.channel.send("Nothing playing to stop.")
-        else:
-            await ctx.channel.send("Music stopped.")
-            await player.stop()
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if not player.is_playing:
+                    ctx.channel.send("Nothing playing to stop.")
+                else:
+                    await ctx.channel.send("Music stopped.")
+                    await player.stop()
+            else:
+                await ctx.channel.send("Please join the same voice channel as me.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
 
     @commands.command(name = 'disconnect', aliases = ['dc'],description="Force disconnects the bot from a voice channel") #disconnects bot from voice channel
     @commands.has_any_role('Dj','Administrator','DJ')
     async def disconnect_bot(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if not player.is_connected:
-            await ctx.channel.send("No bot is connected.")
-        else:
-            await ctx.channel.send("Bot disconnected.")
-            guild_id = int(player.guild_id)
-            await self.connect_to(guild_id,None)
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if not player.is_connected:
+                    await ctx.channel.send("No bot is connected.")
+                else:
+                    await ctx.channel.send("Bot disconnected.")
+                    guild_id = int(player.guild_id)
+                    await self.connect_to(guild_id,None)
+            else: 
+                await ctx.channel.send("Please join the same voice channel as me.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
 
     @commands.command(name='pause',aliases=["ps"],description="Pauses a song if one is playing.") #command to pause currently playing music
     @commands.has_any_role('Dj','Administrator','DJ')
     async def pause_bot(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if player.is_playing:
-            await ctx.channel.send("Song has been paused.")
-            await player.set_pause(True)
-        else:
-            await ctx.channel.send("No song is playing to be paused.")
-
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if player.is_playing:
+                    await ctx.channel.send("Song has been paused.")
+                    await player.set_pause(True)
+                else:
+                    await ctx.channel.send("No song is playing to be paused.")
+            else:
+                await ctx.channel.send("Please join the same voice channel as me.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
     @commands.command(name='unpause', aliases=['resume','start','up'],description="Unpauses a paused song.") #command to unpause currently paused music
     @commands.has_any_role('Dj','Administrator','DJ')
     async def unpause_bot(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if player.paused:
-            await ctx.channel.send("Resuming song.")
-            await player.set_pause(False)
-        else:
-            await ctx.channel.send("Nothing is paused to resume.")
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice.channel.id == int(player.channel_id):
+                if player.paused:
+                    await ctx.channel.send("Resuming song.")
+                    await player.set_pause(False)
+                else:
+                    await ctx.channel.send("Nothing is paused to resume.")
+            else:
+                await ctx.channel.send("Please join the same voice channel as me.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
 
 #    @commands.command(name='current',aliases = ['np','nowplaying'],description="Shows the currently playing song.") #display the currently playing song
@@ -167,29 +198,40 @@ class music(commands.Cog):
     @commands.command(name = "shuffle",description = "Indefinetely shuffles the songs to be played.")
     @commands.has_any_role("Dj","DJ","Administrator")
     async def shuffle(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if player.is_playing:
-            try:
-                player.shuffle = True
-                await ctx.channel.send("Currently playing has been shuffled.")
-            except Exception as error:
-                print(error)
-        else:
-            await ctx.channel.send("No music playing.")
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if player.is_playing:
+                    try:
+                        player.shuffle = True
+                        await ctx.channel.send("Currently playing has been shuffled.")
+                    except Exception as error:
+                        print(error)
+                else:
+                    await ctx.channel.send("No music playing.")
+            else:
+                await ctx.channel.send("Please join my channel to shuffle.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
 
     @commands.command(name = "stopshuffle",aliases = ["unshuffle"],description="Ends the shuffling of all songs.")
     @commands.has_any_role("Dj","DJ","Administrator")
     async def unshuffle(self,ctx):
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        if player.is_playing:
-            try:
-                player.shuffle = False
-                await ctx.channel.send("Currently playing has been unshuffled")
-                #can use channel id to make sure command sender is in same voice chat###
-            except Exception as error:
-                print(error)
-                #NEED CLEAR COMMAND AND DESCRIPTIONS
-        else:
-            await ctx.channel.send("No music is playing.")
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if ctx.author.voice is not None and ctx.author.voice.channel.id == int(player.channel_id):
+                if player.is_playing:
+                    try:
+                        player.shuffle = False
+                        await ctx.channel.send("Currently playing has been unshuffled")
+                    except Exception as error:
+                        print(error)
+                else:
+                    await ctx.channel.send("No music is playing.")
+            else:
+                await ctx.channel.send("Please join my channel to unshuffle.")
+        except Exception as error:
+            await ctx.channel.send("Nothing playing.")
+
 def setup(bot):
     bot.add_cog(music(bot))
