4a262fe10f692c5a43b3cf1cadb7336bbf276d12
RobertAndion <randion@usf.edu>
2020-10-17 15:38:05 -0400

    ReadMe Update
    This was an update to add information about the rename playlist function and of the new welcome features the bot has to offer.

--- welcome.py
+++ welcome.py
@@ -14,7 +14,7 @@ class welcome(commands.Cog):
         servername = str(member.guild) # Grabs the server name
         role = get(member.guild.roles, name = serverrolename) #Remove this line and below to not add a role to a new user
         await member.add_roles(role)                          #Remove me if you remove the line above.
-        channel = get(member.guild.text_channels, name = 'general') # Changed 'general' to the exact channel you want the message to be sent to.
+        channel = get(member.guild.text_channels, name = 'general') # Change 'general' to the exact channel you want the message to be sent to.
         await channel.send("Hello! " + str(member.display_name) + ", welcome to " + servername + "!") #You can customize this message.
         
 
