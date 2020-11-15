6942951e249fd3f224bf3bfad503b1fcdfd4c246
Robert <randion@usf.edu>
2020-11-14 20:55:19 -0500

    Reboot Update
    This allows for rebooting the bot through discord without the need to log into the vps server. Let me know if you find any issues and thoroughly read the Readme to understand how this works.

---
+++ autorestart.sh
@@ -0,0 +1,1 @@
+tmux new-session -d 'sh kill.sh && sleep 5 && sh startup.sh'
--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 import os
 import discord
-import random
+import random # Not sure if this is in use...
+import subprocess #New for the restart command
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 from discord.ext import commands
-intents = discord.Intents.default() # New requirement for the welcome function
+intents = discord.Intents.default()
 intents.members = True
 ## Robert A. USF Computer Science: 
 ## Python discord bot with limited administration commands and an array of
 ## music playing commands, along with a new welcome/auto role function.
 
 Serverinformation = """
-Discord Music Bot Version 1.1.1:
+Discord Music Bot Version 1.1:
 All commands need the prefix "." in order to run. use .help to see all commands and .help play ,
 for example will show information about the play command.
 To use the "Admin" functions the user must have "kick" "ban" permissions for some or a role by the name of "Administrator" for others.
@@ -21,7 +22,7 @@ To use the "Music" functions of the bot the role of: "Dj", "DJ", or "Administrat
 To make roles you enter server settings and click roles. From there the plus sign creates the new role.
 
 """
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='.',intents=intents)
 
 @client.event
 async def on_ready():
@@ -35,5 +36,12 @@ async def on_ready():
 @commands.has_permissions(administrator=True)
 async def setup(ctx):
     await ctx.author.send(Serverinformation)
+
+@client.command(name="reboot")
+@commands.has_permissions(administrator=True)
+async def reboot(ctx):
+    await ctx.channel.send("Rebooting")
+    subprocess.call(["sh","./autorestart.sh"])
+
 
 client.run(TOKEN)
