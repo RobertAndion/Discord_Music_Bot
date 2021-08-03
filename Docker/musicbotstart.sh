40f7d08e70e5a5e33f1838db5c208fe4101da364
Robert <randion@usf.edu>
2021-08-03 17:27:15 -0400

    Docker file write permission fix

--- Docker/musicbotstart.sh
+++ Docker/musicbotstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker container start musicbot
 sleep 5
-docker exec -d musicbot tmux new-session -d 'cd /MusicBot/ && sh startup.sh'
+docker exec -d --user=root musicbot tmux new-session -d 'cd /MusicBot/ && sh startup.sh'
